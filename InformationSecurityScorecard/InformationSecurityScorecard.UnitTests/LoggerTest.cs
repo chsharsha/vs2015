6f32dbb310189818ff6f49380d736df94dc90702
chsharsha <chshandroid@gmail.com>
2015-11-20 19:59:14 -0500

    RDBMS Project- InformationSecurityScorecard

---
+++ InformationSecurityScorecard/InformationSecurityScorecard.UnitTests/LoggerTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InformationSecurityScorecard.UnitTests
+{
+    [TestClass]
+    public class LoggerTest
+    {
+        [TestMethod]
+        public void TestInfoLogging()
+        {
+            InformationSecurityScorecard.Logging.Logger.LogMessage("Logging from Test");
+        }
+    }
+}
