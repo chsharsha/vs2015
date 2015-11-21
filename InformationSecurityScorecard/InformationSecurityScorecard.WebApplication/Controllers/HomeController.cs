ece7f988d9e9ca69e77f9054842780fe81cf58e2
chsharsha <chshandroid@gmail.com>
2015-11-20 20:27:07 -0500

    Added web app

---
+++ InformationSecurityScorecard/InformationSecurityScorecard.WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InformationSecurityScorecard.WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            if(Request.QueryString.HasKeys())
+            {
+                return View();
+            }
+            else
+            {
+                return View("Error");
+            }
+            
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
