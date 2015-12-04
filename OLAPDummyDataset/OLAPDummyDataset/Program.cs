5e02f449609586003b3fe7f92d66347ccdbc4cb4
chsharsha <chshandroid@gmail.com>
2015-12-03 23:21:39 -0500

    al

--- OLAPDummyDataset/OLAPDummyDataset/Program.cs
+++ OLAPDummyDataset/OLAPDummyDataset/Program.cs
@@ -8,13 +8,28 @@ using System.Threading.Tasks;
 
 namespace OLAPDummyDataset
 {
+
+    public class Name
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+
+   
     class Program
     {
         static void Main(string[] args)
         {
+            var locs = ToolKit.LoadUpLocations();
+            Random R=new Random();
             List<string> allFirstNames = new List<string>();
             List<string> allLastNames = new List<string>();
             Regex rgx = new Regex("[^a-zA-Z0-9 -]");
+            string[] GenderArray = new string[] { "M", "F","M","F" };
+            string[] Placedornot = new string[] { "Y", "N","Y","N" };
+            string[] emailDomainArray = new string[] {"@buffalo.edu","@gmail.com","@yahoo.com","@hotmail.com" };
+            string[] semEnrollmentArray = new string[] { "Fall","Spring","Summer"};
             using (StreamReader reader = new StreamReader(@"E:\Data\uniwuenames.txt"))
             {
                 string line;
@@ -55,14 +70,47 @@ namespace OLAPDummyDataset
             var query = from x in allFirstNames
                         from y in allLastNames
                         select new { x, y };
-
-            foreach(var m in query)
+            List<Name> namesList = new List<Name>();
+            query.ToList().ForEach(l => namesList.Add(new Name { FirstName = l.x, LastName = l.y }));
+            StudentMain sm;
+            var filename = @"E:\Data\DummyFileOLAP" + new Random().Next(0, 450) + ".csv";
+            using (FileStream fs = File.Create(filename))
             {
-                Console.WriteLine(m.x + " " + m.y);
-            }
+                var header= "StudentID,First_Name,Last_Name,Age,Gender,City,State,Zip,Email,Phone_Number,PlacedYN,Date_Of_Placement,Date_Of_Graduation,Sem_Of_Enrollment,Year_Enrolled,Salary,Pre_Work_Ex";
+                header = header + System.Environment.NewLine;
+                Byte[] info = new UTF8Encoding(true).GetBytes(header);
 
-            Console.Write(query.Count());
-            Console.ReadKey();
+                fs.Write(info, 0, info.Length);
+                foreach (var name in namesList)
+                {
+
+
+                    sm = new StudentMain();
+                    sm.StudentID= ToolKit.RandomString(12, R);
+                    sm.FirstName = name.FirstName;
+                    sm.LastName = name.LastName;
+                    sm.Age = R.Next(18, 46);
+                    sm.Gender = GenderArray[R.Next(0, GenderArray.Length)];
+                    Loc l = new Loc();
+                    l = locs[R.Next(0, locs.Length)];
+                    sm.City = l.City;
+                    sm.State = l.State;
+                    sm.Zip = l.Zip;
+                    sm.Email = sm.FirstName + "." + sm.LastName + emailDomainArray[R.Next(0, emailDomainArray.Length)];
+                    sm.PhoneNo = ToolKit.RandomString(10, R);
+                    sm.PlacedYN = Placedornot[R.Next(0,Placedornot.Length)];
+                    sm.YearEnrolled = R.Next(2014, 2016);
+                    sm.PlacementDate = (sm.PlacedYN == "N") ? DateTime.MinValue : ToolKit.RandomDay(R, sm.YearEnrolled);
+                    sm.GraduationDate = ToolKit.RandomDay(R, sm.YearEnrolled);
+                    sm.EnrolledSEM = semEnrollmentArray[R.Next(0, semEnrollmentArray.Length)];
+                    sm.Salary =(sm.PlacedYN=="N")?0: R.Next(70000, 130001);
+                    sm.PreWorkex = R.Next(0, 11);
+                    info = new UTF8Encoding(true).GetBytes(ToolKit.GiveRecord(sm));
+
+                    fs.Write(info, 0, info.Length);
+                }
+            }
+           
         }
     }
 }
---
+++ OLAPDummyDataset/OLAPDummyDataset/StudentMain.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OLAPDummyDataset
+{
+
+    public class Loc
+    {
+        public string City { get; set; }
+
+        public string Zip { get; set; }
+
+        public string State { get; set; }
+
+    }
+    public class StudentMain
+    {
+        public string StudentID { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string Gender { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Zip { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNo { get; set; }
+
+        public string PlacedYN { get; set; }
+
+        public DateTime PlacementDate { get; set; }
+
+        public DateTime GraduationDate { get; set; }
+
+        public string EnrolledSEM { get; set; }
+
+        public int YearEnrolled { get; set; }
+
+        public int Salary { get; set; }
+
+        public int PreWorkex { get; set; }
+    }
+
+
+    public static class ToolKit
+    {
+
+        public static DateTime RandomDay(Random gen, int yearEnrolled)
+        {
+            DateTime start = new DateTime(2015, 1, 1);
+            DateTime end = new DateTime(2016, 12, 31);
+            DateTime start16 = new DateTime(2016, 1, 1);
+            if (yearEnrolled == 2015)
+            {
+                int range = (end - start16).Days;
+                return start16.AddDays(gen.Next(range));
+            }
+            else
+            {
+                int range = (end - start).Days;
+                return start.AddDays(gen.Next(range));
+            }
+
+        }
+
+
+        public static string GiveRecord(StudentMain sm)
+        {
+            string sb = "";
+            sb = sb + sm.StudentID + ",";
+            sb = sb + sm.FirstName + ",";
+            sb = sb + sm.LastName + ",";
+            sb = sb + sm.Age.ToString() + ",";
+            sb = sb + sm.Gender + ",";
+            sb = sb + sm.City + ",";
+            sb = sb + sm.State + ",";
+            sb = sb + sm.Zip + ",";
+            sb = sb + sm.Email + ",";
+            sb = sb + sm.PhoneNo + ",";
+            sb = sb + sm.PlacedYN + ",";
+            if(sm.PlacementDate==DateTime.MinValue)
+            {
+                sb = sb + string.Empty + ",";
+            }
+            else
+            {
+                sb = sb + sm.PlacementDate.ToShortDateString() + ",";
+            }
+            sb = sb + sm.GraduationDate.ToShortDateString() + ",";
+            
+            sb = sb + sm.EnrolledSEM + ",";
+            sb = sb + sm.YearEnrolled + ",";
+            if (sm.Salary == 0)
+            {
+                sb = sb + string.Empty + ",";
+            }
+            else
+            {
+                sb = sb + sm.Salary.ToString() + ",";
+            }
+            sb = sb + sm.PreWorkex.ToString();
+
+            sb = sb + System.Environment.NewLine;
+            return sb;
+
+        }
+        public static string RandomString(int Size, Random r)
+        {
+            string input = "0123456789";
+            var chars = Enumerable.Range(0, Size)
+                                   .Select(x => input[r.Next(0, input.Length)]);
+            return new string(chars.ToArray());
+        }
+        public static Loc[] LoadUpLocations()
+        {
+            List<Loc> locList = new List<Loc>();
+            locList.Add(new Loc { City = "00501", Zip = "Holtsville", State = "NY" });
+            locList.Add(new Loc { City = "00544", Zip = "Holtsville", State = "NY" });
+            locList.Add(new Loc { City = "10001", Zip = "New York", State = "NY" });
+            locList.Add(new Loc { City = "10001", Zip = "New York", State = "NY" });
+            locList.Add(new Loc { City = "10002", Zip = "New York", State = "NY" });
+            locList.Add(new Loc { City = "10003", Zip = "New York", State = "NY" });
+            locList.Add(new Loc { City = "10004", Zip = "New York", State = "NY" });
+            locList.Add(new Loc { City = "10005", Zip = "New York", State = "NY" });
+            locList.Add(new Loc { City = "10006", Zip = "New York", State = "NY" });
+            locList.Add(new Loc { City = "10007", Zip = "New York", State = "NY" });
+            locList.Add(new Loc { City = "10008", Zip = "New York", State = "NY" });
+            locList.Add(new Loc { City = "10009", Zip = "New York", State = "NY" });
+            locList.Add(new Loc { City = "10301", Zip = "Staten Island", State = "NY" });
+            locList.Add(new Loc { City = "10302", Zip = "Staten Island", State = "NY" });
+            locList.Add(new Loc { City = "10303", Zip = "Staten Island", State = "NY" });
+            locList.Add(new Loc { City = "10304", Zip = "Staten Island", State = "NY" });
+            locList.Add(new Loc { City = "10305", Zip = "Staten Island", State = "NY" });
+            locList.Add(new Loc { City = "10306", Zip = "Staten Island", State = "NY" });
+            locList.Add(new Loc { City = "10307", Zip = "Staten Island", State = "NY" });
+            locList.Add(new Loc { City = "10308", Zip = "Staten Island", State = "NY" });
+            locList.Add(new Loc { City = "10309", Zip = "Staten Island", State = "NY" });
+            locList.Add(new Loc { City = "10451", Zip = "Bronx", State = "NY" });
+            locList.Add(new Loc { City = "10452", Zip = "Bronx", State = "NY" });
+            locList.Add(new Loc { City = "10453", Zip = "Bronx", State = "NY" });
+            locList.Add(new Loc { City = "10454", Zip = "Bronx", State = "NY" });
+            locList.Add(new Loc { City = "10455", Zip = "Bronx", State = "NY" });
+            locList.Add(new Loc { City = "10456", Zip = "Bronx", State = "NY" });
+            locList.Add(new Loc { City = "10457", Zip = "Bronx", State = "NY" });
+            locList.Add(new Loc { City = "10458", Zip = "Bronx", State = "NY" });
+            locList.Add(new Loc { City = "10459", Zip = "Bronx", State = "NY" });
+            locList.Add(new Loc { City = "14214", Zip = "Buffalo", State = "NY" });
+            locList.Add(new Loc { City = "14260", Zip = "Buffalo", State = "NY" });
+            return locList.ToArray();
+        }
+    }
+}
