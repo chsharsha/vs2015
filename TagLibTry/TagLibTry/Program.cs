3ffb71f494dfa293a923a8ca77a024fab549d16a
chsharsha <chshandroid@gmail.com>
2016-02-08 21:40:04 -0500

    Tag lib trying

---
+++ TagLibTry/TagLibTry/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagLib;
+namespace TagLibTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TagLib.File f = TagLib.File.Create(@"F:\Downloads\04 - Chalaaki Chilipi - [www.MaaMp3.com].mp3");
+            f.Tag.Album = "Lets Try";
+            f.Save();
+        }
+    }
+}
